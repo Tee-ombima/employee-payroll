8912f0efea4e5f0056a4fb1c9184f62a5625a900
Tee-ombima <ombimatitus7@gmail.com>
2025-08-03 11:06:24 +0100

    switched from vite to mix asset compiler for prod

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', config('app.name'))</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+<script src="{{ asset('js/app.js') }}" defer></script>
 
 </head>
 <body class="font-sans antialiased">
